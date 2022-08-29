886365877690f1eb8636ff660935356086647a73
saleknia <salekniaamirhossein@gmail.com>
2022-08-29 16:16:21 +0000

    Commit Process

---
+++ requirements.py
@@ -0,0 +1,17 @@
+import os
+os.system('pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html')
+os.system('pip install logging')
+os.system('pip install scipy==1.5.4')
+os.system('pip install numpy')
+os.system('pip install h5py')
+os.system('pip install wget')
+os.system('pip install tensorboardX==2.4')
+os.system('pip install SimpleITK==2.1.1')
+os.system('pip install MedPy==0.4.0')
+os.system('tensorboardX==1.8')
+os.system('tensorboard==1.10.0')
+os.system('tqdm==4.30.0')
+
+
+
+
